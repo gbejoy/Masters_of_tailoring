2247dfb39ace6a61f932b5d970bbd77cf24a0131
Luke1836 <codingluke2023@gmail.com>
2024-10-08 19:42:06 +0530

    Loading page 1

--- vite-project/src/Components/LandingPage.jsx
+++ vite-project/src/Components/LandingPage.jsx
@@ -8,6 +8,10 @@ function LandingPage()
             <div className="h-svh w-full bg=[#17383E]">
                 <img src="/Landing_Page.png" alt="landing page" className="h-full w-full object-cover lg:object-fill" />
             </div>
+            <div className="flex flex-col gap-2 text-center w-[35rem] absolute top-1/2 left-1/2 text-stone-100 -translate-x-1/2 -translate-y-1/2 bg-[#1313132c] backdrop-blur-64">
+                <h1 className="text-5xl font-bold shadow-text">Masters of <span className="text-[#F28928]">Tailoring</span></h1>
+                <p className="text-[#66E7E1] font-bold shadow-text-light">Crafting Excellence, One Stitch at a Time</p>
+            </div>
         </div>
     )
 }

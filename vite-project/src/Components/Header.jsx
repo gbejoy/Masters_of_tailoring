dcf29eea7d9d4773e0eeb41a7d7431f9ca9f36c1
Luke1836 <codingluke2023@gmail.com>
2024-10-08 19:26:23 +0530

    Header componenet styled 2

--- vite-project/src/Components/Header.jsx
+++ vite-project/src/Components/Header.jsx
@@ -3,9 +3,9 @@ import Logo from "/Website-logo.png"
 function Header()
 {
     return (
-        <header className="">
-            <div className="">
-                <img src={ Logo } alt="Company's logo" />
+        <header className="w-full h-[5rem] absolute top-0 left-0">
+            <div className="h-16 w-16 ">
+                <img src={ Logo } alt="Company's logo" className="text-center" />
             </div>
         </header>
     )
--- vite-project/src/Components/LandingPage.jsx
+++ vite-project/src/Components/LandingPage.jsx
@@ -3,7 +3,12 @@ import Header from "./Header";
 function LandingPage()
 {
     return (
-        <Header />
+        <div className="w-full h-full">
+            <Header />
+            <div className="h-svh w-full bg=[#17383E]">
+                <img src="/Landing_Page.png" alt="landing page" className="h-full w-full object-cover lg:object-fill" />
+            </div>
+        </div>
     )
 }
 

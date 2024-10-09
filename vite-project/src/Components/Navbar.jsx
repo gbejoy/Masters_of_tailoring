d9c338ae4f03d3e417b44ab3e51a164dccc0666c
Luke1836 <codingluke2023@gmail.com>
2024-10-09 19:44:15 +0530

    Navbar styling-1

--- vite-project/src/Components/Navbar.jsx
+++ vite-project/src/Components/Navbar.jsx
@@ -4,8 +4,21 @@ function Navbar({ isClicked, isLogged })
     const btnStyles = "text-stone-100"
 
     return (
-        <div className={`absolute top-full left-0 w-full bg-[#13131334] backdrop-blur-[4px] md:hidden ${isClicked ? 'flex' : 'hidden'} flex-col items-center`}>
-            <dl className="flex flex-col items-center justify-center gap-2 p-4">
+        <div className={`absolute top-full right-0 w-3/4 h-[calc(100svh-5rem)] bg-[#13131396] backdrop-blur-[4px] md:hidden ${isClicked ? 'flex' : 'hidden'} flex-col items-start pl-[0.5rem]`}>
+            <div className="text-stone-100 flex flex-row gap-2 pl-[1rem] lg:pl-[2rem]">
+                {!isLogged ? (
+                    <>
+                        <button className={btnStyles}>Login</button>
+                        <button className={btnStyles}>Sign Up</button>
+                    </>
+                ) : (
+                    <div className="">
+                        <button className=""></button>
+                        <button className=""></button>
+                    </div>
+                )}
+            </div>
+            <dl className="flex flex-col gap-2 p-4">
                 <dd>
                     <a href="#about" className={navItems}>
                         About
@@ -27,19 +40,6 @@ function Navbar({ isClicked, isLogged })
                     </a>
                 </dd>
             </dl>
-            <div className="text-stone-100 flex flex-row gap-2 pl-[1rem] lg:pl-[2rem]">
-                {!isLogged ? (
-                    <>
-                        <button className={btnStyles}>Login</button>
-                        <button className={btnStyles}>Sign Up</button>
-                    </>
-                ) : (
-                    <div className="">
-                        <button className=""></button>
-                        <button className=""></button>
-                    </div>
-                )}
-            </div>
         </div>
     )
 }

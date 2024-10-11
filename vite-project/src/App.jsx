3ca1463c21f9e735fa09b08ed5573b469a696775
gbejoy <imgokulbejoy@gmail.com>
2024-10-11 11:16:30 +0530

    Added Firebase configuration and initialized it.

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,23 @@ import LandingPage from "./Components/LandingPage";
 import About from "./Components/About.jsx";
 import Masterpieces from "./Components/Masterpieces.jsx";
 import Collections from "./Components/Collections.jsx";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDpDHRqZKsXAENAL4Zh3Imb5Th_F7vndU",
+  authDomain: "masteroftailoring-53370.firebaseapp.com",
+  projectId: "masteroftailoring-53370",
+  storageBucket: "masteroftailoring-53370.appspot.com",
+  messagingSenderId: "720844154178",
+  appId: "1:720844154178:web:dd1c98aa19d50e603994db",
+  measurementId: "G-XFV5FKP3XE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App()
 {
